1158d8a9dcfb7f3d4c28ee4bf5225fb9d58a3c61
Selena-512 <melodyhlkwok@gmail.com>
2024-05-10 17:22:16 +0100

    no form

--- people-search-js.js
+++ people-search-js.js
@@ -1,5 +1,7 @@
+//import supabase client
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
+//init supabase url and key
 const supabaseUrl = 'https://akzmofevgcjpjcwmkjld.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrem1vZmV2Z2NqcGpjd21ramxkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4NTA2MzcsImV4cCI6MjAyODQyNjYzN30.2my4atOnBxXTukJohfomcQSphSVmcZj7Jp1FQRhWuNA';
 const supabase = createClient(supabaseUrl, supabaseKey);
@@ -20,12 +22,6 @@ async function fetchData(lnum) {
 
     return data;
 }
-
-
-
-//THERE SHOULD BE STUFF HERE :D
-
-
 
 
 
