ed442c098795d59ea82afc11f38f8bed96e9f506
Selena-512 <melodyhlkwok@gmail.com>
2024-05-10 18:25:40 +0100

    btn change

--- people-search-js.js
+++ people-search-js.js
@@ -6,33 +6,18 @@ const supabaseUrl = 'https://akzmofevgcjpjcwmkjld.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrem1vZmV2Z2NqcGpjd21ramxkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4NTA2MzcsImV4cCI6MjAyODQyNjYzN30.2my4atOnBxXTukJohfomcQSphSVmcZj7Jp1FQRhWuNA';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-const dname = document.getElementById("dname");
-const lnum = document.getElementById("lnum");
+const nameInput = document.getElementById("name");
+const lnumInput = document.getElementById("license");
+const msgDiv = document.getElementById("message");
+const resultDiv = documment.getElementById("result");
 
-async function fetchData(lnum) {
-    const { data, error } = await supabase
-        .from('People')
-        .select("*")
-        .or(`LicenseNumber.ilike.%${lnum}%`);
+const submitBtn = document.getElementById("submit");
+btn1.addEventListener("click", submit);
 
-    if (error) {
-        console.error('Error Fetching Data:', error.message);
-        return [];
-    }
+//license number input
+async function fetchData(lnumInput) {
+    const { data, error} = await supabase.from('People').select();
 
-    return data;
-}
-
-
-
-const { data:Data2, error } = await supabase
-.from('People')
-.select("*")
-.eq(`PersonID`, OwnerID);
-
-if (error) {
-    console.error('Error Fetching Data:', error.message);
-    return [];
 }
 
 
