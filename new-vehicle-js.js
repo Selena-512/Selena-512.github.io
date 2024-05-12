79d7a8625b94aa05940ae718a78b789f5fd02bcb
Selena-512 <melodyhlkwok@gmail.com>
2024-05-13 01:03:58 +0800

    v0.42
    added new vehicle js to new vehicle html
    added basic constants and variables to new vehicle javascriptt
    first try on addVehicle function

---
+++ new-vehicle-js.js
@@ -0,0 +1,39 @@
+//import supabase client
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+//init supabase url and key
+const supabaseUrl = 'https://akzmofevgcjpjcwmkjld.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFrem1vZmV2Z2NqcGpjd21ramxkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI4NTA2MzcsImV4cCI6MjAyODQyNjYzN30.2my4atOnBxXTukJohfomcQSphSVmcZj7Jp1FQRhWuNA';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+const regoInput = document.getElementById("rego");
+const makeInput = document.getElementById("make");
+const modelInput = document.getElementById("model");
+const colourInput = document.getElementById("colour");
+const ownerInput = document.getElementById("owner");
+const personidInput = document.getElementById("personid");
+const nameInput = document.getElementById("name");
+const addressInput = document.getElementById("address");
+const dobInput = document.getElementById("dob");
+const licenseInput = document.getElementById("license");
+const expireInput = document.getElementById("expire");
+const msgDiv = document.getElementById("message");
+
+const addVehicleBtn = document.getElementById("addvehicle");
+addVehicleBtn.addEventListener("click", addVehicle);
+
+const addOwnerBtn = document.getElementById("addowner");
+//addOwnerBtn.addEventListener("click", submit);
+
+async function addVehicle(){
+    const { error } = await supabase
+        .from('Vehicles')
+        .insert({ 
+            VehicleID: regoInput.value,
+            Make: makeInput.value,
+            Model: modelInput.value,
+            Colour: colourInput.value,
+            OwnerID: ownerInput.value
+        });
+    msgDiv.innerHTML += "[" + regoInput.value +"] Vehicle added successfully"
+}
