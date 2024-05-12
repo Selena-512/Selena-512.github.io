93771a33caadbb8b35582f0bc9ec6be9a3f6eee9
Selena-512 <melodyhlkwok@gmail.com>
2024-05-12 16:27:46 +0800

    v0.19
    added people search js to html
    changed id / variables names
    first try fetch all data to console log

--- people-search-js.js
+++ people-search-js.js
@@ -7,18 +7,18 @@ const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const nameInput = document.getElementById("name");
-const lnumInput = document.getElementById("license");
+const licenseInput = document.getElementById("license");
 const msgDiv = document.getElementById("message");
 const resultDiv = documment.getElementById("result");
 
 const submitBtn = document.getElementById("submit");
-btn1.addEventListener("click", submit);
+submitBtn.addEventListener("click", fetchData);
 
 //license number input
-async function fetchData(lnumInput) {
-    const { data, error} = await supabase.from('People').select();
+async function fetchData() {
+    const { data, error} = await supabase
+        .from('People')
+        .select('*');
     console.log('fetched data:',data);
 }
-
-fetchData();
 
